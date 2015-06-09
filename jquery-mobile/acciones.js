afaaba610a02b02a94837da0f360b488ea5af457
GabovanBeethoven <gaboexodia5@gmail.com>
2015-06-08 20:19:43 -0500

    Animales
    Sonidos

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(document).ready(function(e){
+	var caballo = document.getElementById(gallina);
+	var gallina = document.getElementById(gallina);
+	var Gallo = document.getElementById(gallina);
+	var gatito = document.getElementById(gallina);
+	var Perro = document.getElementById(gallina);
+	var Burro = document.getElementById(gallina);
+	
+	var Aguila = document.getElementById(gallina);
+	var Gorila = document.getElementById(gallina);
+	var Leon = document.getElementById(gallina);
+	var Lobo = document.getElementById(gallina);
+	var Oso = document.getElementById(gallina);
+	var Pantera = document.getElementById(gallina);
+});//ready
